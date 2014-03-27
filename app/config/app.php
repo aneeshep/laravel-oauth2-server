39cc82dff0968ab826c4e67e01a21f0527ebb5a5
Aneesh Kumar <aneesh.e@imaginea.com>
2014-03-27 22:29:12 +0530

    Installed Sentry and Generators

--- app/config/app.php
+++ app/config/app.php
@@ -106,6 +106,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Cartalyst\Sentry\SentryServiceProvider',
+		'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
@@ -172,6 +174,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Sentry'		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 
 	),
 
--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'school_management',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
