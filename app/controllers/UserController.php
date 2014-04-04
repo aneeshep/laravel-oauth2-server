b522354f9a6c9b47036bad2dc6535394d0df0d9c
Aneesh Kumar <aneesh.e@imaginea.com>
2014-04-04 17:51:38 +0530

    Added Signup

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -29,7 +29,89 @@ class UserController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$input = Input::all();
+
+		//validate the input
+		/*
+		 * To be done
+		 * Create a Custom Validate helper. Dont make the controller complex
+		*/
+
+		try
+		{
+		    // Create the user
+		    $user = Sentry::createUser($input);
+
+		    // Find the group using the group id
+		    $adminGroup = Sentry::findGroupByName('admin');
+
+		    // Assign the group to the user
+		    $user->addGroup($adminGroup);
+
+		    return Response::json(array(
+                'status' => 200,
+                'error' => '',
+                'message' => 'Signup Success',
+            ), 200);
+		}
+		catch (Cartalyst\Sentry\Users\LoginRequiredException $e)
+		{
+		    //echo 'Login field is required.';
+		    return Response::json(array(
+                'status' => 400,
+                'error' => 'MissingRequiredQueryParameter',
+                'error_message' => $e->getMessage(),
+            ), 400);
+		}
+		catch (Cartalyst\Sentry\Users\PasswordRequiredException $e)
+		{
+		    //echo 'Password field is required.';
+		    return Response::json(array(
+                'status' => 400,
+                'error' => 'MissingRequiredQueryParameter',
+                'error_message' => $e->getMessage(),
+            ), 400);
+		}
+		catch (Cartalyst\Sentry\Users\UserExistsException $e)
+		{
+		    //echo 'User with this login already exists.';
+		    return Response::json(array(
+                'status' => 409,
+                'error' => 'AccountAlreadyExists',
+                'error_message' => $e->getMessage(),
+            ), 409);
+		}
+		catch (Cartalyst\Sentry\Groups\GroupNotFoundException $e)
+		{
+		    //echo 'Group was not found.';
+		    return Response::json(array(
+                'status' => 404,
+                'error' => 'ResourceNotFound',
+                'error_message' => $e->getMessage(),
+            ), 404);
+		}
+
+		catch(Illuminate\Database\QueryException $e)
+		{
+			//print_r($e->getCode());
+
+			if($e->getCode() == 23000)
+			{
+				return Response::json(array(
+	                'status' => 409,
+	                'error' => 'AccountAlreadyExists',
+	                'error_message' => 'Username/Email already exists',
+	            ), 409);
+			}
+			else {
+
+				return Response::json(array(
+	                'status' => 500,
+	                'error' => 'InternalServerError',
+	                'error_message' => 'Oops!. Something went Wrong',
+	            ), 500);
+			}
+		}
 	}
 
 	/**
